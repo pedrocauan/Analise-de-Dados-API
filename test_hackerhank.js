08b2441b97ab7c35b48ea68b48adc0a796e31744
Pedro Cauan <90540372+pedrocauan@users.noreply.github.com>
2023-04-09 21:20:40 -0300

    Update test_hackerhank.js

--- test_hackerhank.js
+++ test_hackerhank.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 async function getCountryName(code) {
-    let country = "NOT FOUND";
+    let country = "NOT FOUND"; //guarda o país
     //Procura todas as paginas de países (25 paginas no total)
     for(let i = 1; i < 26; i++){
         const countries = await axios(`https://jsonmock.hackerrank.com/api/countries?page=${i}`);
         //percorre todos os países de todas as páginas
         countries.data.data.forEach(c => {
-            //retorna o país com a sigla informada
+            //pega o país com a sigla informada
             if(code === c.alpha2Code) {
                 country = c.name;
                 return;
@@ -16,6 +16,8 @@ async function getCountryName(code) {
         //sai da iteração quando encontra o item
         if(country !== "NOT FOUND") break;
     }
+    
+    return country /*Retorna NOT FOUND se a sigla nao existe ou o país caso a sigla exista*/
 }
 
 async function main() {
